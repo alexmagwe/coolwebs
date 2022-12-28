4c5db515514cfec9016d63b74e0c9c5061fff33e
alexmagwe <alexmagwe@gmail.com>
2022-12-28 14:32:00 +0300

    update:updated client db url

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,10 @@
-import {ApolloClient,InMemoryCache} from '@apollo/client'
-const apolloClient=new ApolloClient({
-    uri:"http://localhost:3000/api/graphql",
-    credentials:'same-origin',
-    cache:new InMemoryCache()
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+const apolloClient = new ApolloClient({
+  uri:
+    process.env.NODE_ENV == 'production'
+      ? 'https://coolwebs.vercel.app/api/graphql'
+      : 'http://localhost:3000/api/graphql',
+  credentials: 'same-origin',
+  cache: new InMemoryCache(),
 })
 export default apolloClient
